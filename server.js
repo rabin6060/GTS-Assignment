252581fd143380bc5b69d3c6198c4b5c19de2a4d
Rabin Mainali <rabinmainali1999@gmail.com>
2025-04-17 11:20:28 +0545

    middlewares added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const productRoute = require('./routes/product.routes')
 
 const app = express()
-
 const port = 3000
 
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/products',productRoute)
 
 app.listen(port,()=>{
     console.log(`server running at port ${port} `)
